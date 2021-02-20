a6d828d66a68b9a86d881ec2987bafc889f804bf
vmarceau <vmarceau@gmail.com>
2021-02-20 01:28:45 -0500

    Optimize no 5

--- euler/lib/euler05.ex
+++ euler/lib/euler05.ex
@@ -1,18 +1,29 @@
 defmodule Euler05 do
   @moduledoc """
-  Problem 4: What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
-  This solution is ugly... There's a much more clever way to solve this but I'm too lazy to optimize and that's not the point here...
+  Problem 5: What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+  This solution is not optimal, but I'm too lazy to optimize...
   """
   @spec solve :: number
   def solve do
-    find(19 * 20)
+    step = 1..20 |> Enum.filter(&prime?(&1)) |> Enum.reduce(1, &(&1 * &2))
+    find(step, step)
   end
 
-  def find(n) do
-    if Enum.reduce_while(1..18, true, fn x, acc ->
+  defp find(n, step) do
+    if Enum.reduce_while(1..20, true, fn x, acc ->
          if rem(n, x) == 0, do: {:cont, acc}, else: {:halt, false}
        end),
        do: n,
-       else: find(n + 19 * 20)
+       else: find(n + step, step)
+  end
+
+  defp prime?(n) when n in [2, 3], do: true
+
+  defp prime?(n) do
+    max = div(n, 2)
+
+    Enum.reduce_while(2..max, true, fn x, acc ->
+      if rem(n, x) == 0, do: {:halt, false}, else: {:cont, acc}
+    end)
   end
 end
